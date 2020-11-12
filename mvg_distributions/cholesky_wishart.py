2969bc158502bbcf9abee78cd860c4cde64b6aa7
Stefan Brugger <stefpb@gmail.com>
2020-11-12 22:42:01 +0000

    fix imports

--- mvg_distributions/cholesky_wishart.py
+++ mvg_distributions/cholesky_wishart.py
@@ -2,7 +2,8 @@ import tensorflow as tf
 from mvg_distributions import covariance_representations as cov_rep
 import numpy as np
 #from tensorflow_probability.python.distributions import seed_stream
-from tensorflow_probability.util import SeedStream
+import tensorflow_probability as tfp
+from tfp.util import SeedStream
 
 
 class CholeskyWishart(tf.distributions.Distribution):
